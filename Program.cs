ddc2bfb3357de2e345a03f94896a4856f29ce6ae
Russell Wallace <russell.wallace@gmail.com>
2023-11-11 22:19:23 +0000

    test

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 
 var stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < 100000000; i++)
+    _ = "aBcDeFgH".ToLowerInvariant();
 stopwatch.Stop();
 Console.WriteLine(stopwatch.Elapsed);
